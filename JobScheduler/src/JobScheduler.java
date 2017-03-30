e0ca45151ac76520e54f5d5e3e645fa5e28e1e6b
Michael <michaelharootoonyan@gmail.com>
2017-03-29 18:23:19 -0700

    init commit

---
+++ JobScheduler/src/JobScheduler.java
@@ -0,0 +1,54 @@
+
+public class JobScheduler
+{
+    private int nJobs;
+    private Job[]  jobs;
+
+    public JobScheduler( int[] joblength, int[] deadline, int[] profit)
+    {
+//Set nJobs
+//Fill jobs array. The kth job entered has JobNo = k;
+    }
+
+    public void printJobs()  //prints the array jobs
+    {  }
+
+    //Brute force. Try all n! orderings. Return the schedule with the most profit
+    public Schedule bruteForceSolution()
+    {
+        return null;
+    }
+
+
+    public Schedule makeScheduleEDF()
+//earliest deadline first schedule. Schedule items contributing 0 to total profit last
+    {
+
+        Schedule schedule = new Schedule();
+        Sorts.mergesort(jobs.deadline)
+        for (int i = 0; i < jobs.length; i++) {
+            schedule.add(jobs[i]);
+        }
+        return schedule;
+    }
+
+    public Schedule makeScheduleSJF()
+//shortest job first schedule. Schedule items contributing 0 to total profit last
+    {
+        return null;
+    }
+
+    public Schedule makeScheduleHPF()
+//highest profit first schedule. Schedule items contributing 0 to total profit last
+    {
+
+        return null;
+    }
+
+
+    public Schedule newApproxSchedule() //Your own creation. Must be <= O(n3)
+    {
+        return null;
+    }
+
+}//end of JobScheduler class
