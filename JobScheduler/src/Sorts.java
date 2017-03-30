2e37b67003120870733051e30af62d59f587c95c
michaelh818 <michaelharootoonyan@gmail.com>
2017-03-29 19:38:40 -0700

    Cleaned up code and fixed mergesort

--- JobScheduler/src/Sorts.java
+++ JobScheduler/src/Sorts.java
@@ -3,114 +3,85 @@ import javax.jws.soap.SOAPBinding;
 public class Sorts {
 
     private static int INFINITY = 10000000;
-    /*--------------Insertion Sort -----------------------*/
-    public static long insertionsort( int[] a)
-    {
-        long comparisonsCount = 0;
-        for (int j = 1; j < a.length; j++) {
-            int key = a[j];
-            int i = j - 1;
-
-            while (i > -1 && a[i] > key) {
-                comparisonsCount++; // checking loop condition when true
-                a[i+1] = a[i];
-                i = i - 1;
-            }
-            comparisonsCount++; // checking loop condition when false
-            a[i+1] = key;
-        }
-        return comparisonsCount;
-
-    }
-
+    private static int EARILIEST_DEADLINE_FIRST = 0;
+    private static int HIGHEST_PROFIT_FIRST = 1;
+    private static int SHORTEST_JOB_FIRST = 2;
 
 
 /* --------------------Merge Sort --------------------*/
     //merges sorted slices a[i.. j] and a[j + 1 …  k] for 0<=  i <=j < k < a.length
 
-    public static long merge ( Job [] a,  int i, int m , int k, int SORT_BY)
+    public static Job[] merge ( Job [] a,  int i, int m , int k, int SORT_BY)
     {
-        long comparisonsCount = 0;
         int nL = m - i + 1;
         int nR = k - m;
         Job [] L = new Job[nL+1];
         Job [] R = new Job[nR+1];
-        L[nL] = new Job(INFINITY,INFINITY,INFINITY,INFINITY);
-        R[nR] = new Job(INFINITY,INFINITY,INFINITY,INFINITY);
+
+        boolean leftSideLessThanOrEqualToRight = false;
+        
+        // keep this format until brute force and dynamic programming confirms we can use the job(-1, INFINITY, INIFINITY, -1) as a constant call INFINITY_JOB.
+        if (SORT_BY == EARILIEST_DEADLINE_FIRST) {
+            leftSideLessThanOrEqualToRight = L[l].deadline <= R[r].deadline;
+            // Job( int jn , int len, int d, int p)
+            L[nL] = new Job(-1, -1, INFINITY, -1);
+            R[nR] = L[nL];
+        } else if (SORT_BY == HIGHEST_PROFIT_FIRST) {
+            // note the difference
+            leftSideLessThanOrEqualToRight = R[r].profit <= L[l].profit;
+            // Job( int jn , int len, int d, int p)
+            L[nL] = new Job(-1, -1, INFINITY, -1);
+            R[nR] = L[nL];
+        } else if (SORT_BY == SHORTEST_JOB_FIRST) {
+            leftSideLessThanOrEqualToRight = L[l].length <= R[r].length;
+            // Job( int jn , int len, int d, int p)
+            L[nL] = new Job(-1, INFINITY, INFINITY, -1);
+            R[nR] = L[nL];
+        }
 
         // divide left hand side from
         for (int  l = 0; l < nL; l++) {
-            comparisonsCount++;
             L[l] = a[l+i];
         }
 
         // divide right hand side from
         for (int  r = 0; r < nR; r++) {
-            comparisonsCount++;
             R[r] = a[r+m+1];
         }
 
         // merge left and right side in sorted order
         int l = 0, r = 0;
+
         for (int j = i; j <= k; j++) {
-            if(SORT_BY == 0) {
-                if (L[l].deadline <= R[r].deadline) {
-                    comparisonsCount++;// if true
-                    a[j] = L[l];
-                    l++;
-                } else {
-                    comparisonsCount++;// if false
-                    a[j] = R[r];
-                    r++;
-                }
-            }
-            else if(SORT_BY == 1)
-            {
-                if (L[l].profit <= R[r].profit) {
-                    comparisonsCount++;// if true
-                    a[j] = L[l];
-                    l++;
-                } else {
-                    comparisonsCount++;// if false
-                    a[j] = R[r];
-                    r++;
-                }
-            }else if(SORT_BY == 2){
-                if (L[l].length <= R[r].length) {
-                    comparisonsCount++;// if true
-                    a[j] = L[l];
-                    l++;
-                } else {
-                    comparisonsCount++;// if false
-                    a[j] = R[r];
-                    r++;
-                }
+            if (leftSideLessThanOrEqualToRight) {
+                a[j] = L[l];
+                l++;
+            } else {
+                a[j] = R[r];
+                r++;
             }
         }
 
 
-        return comparisonsCount;
+        return a;
     }
 
 
     //sorts  a[ i .. k]  for 0<=i <= k < a.length
-    private  static  long mergesort(Job[] a,  int i ,  int k, int SORT_BY)
+    private static Job[] mergesort(Job[] a,  int i ,  int k, int SORT_BY)
     {
-        int comparisonsCount = 0;
         if( i < k) {
-            comparisonsCount++; // if true
             int m = (i+k)/2;
-            comparisonsCount += mergesort(a,i,m,SORT_BY);
-            comparisonsCount += mergesort(a,m+1,k,SORT_BY);
-            comparisonsCount += merge(a,i,m,k,SORT_BY);
+            a = mergesort(a,i,m,SORT_BY);
+            a = mergesort(a,m+1,k,SORT_BY);
+            a = merge(a,i,m,k,SORT_BY);
         }
-        comparisonsCount++; // if false
-        return comparisonsCount;
+        return a;
     }
 
 
     //Sorts the array using mergesort
-    public static  long mergesort(Job[] a , int SORT_BY)
+    public static Job[] mergesort(Job[] a , int SORT_BY)
     {
         return mergesort(a, 0, a.length - 1,SORT_BY);
     }
